befd00efdd7fe5478d258b6d347af6aa15089ee2
hab <_@habnab.it>
2020-11-05 17:03:46 +0000

    add a couple more test cases

--- pep634regex_test.py
+++ pep634regex_test.py
@@ -11,6 +11,28 @@ def test_considering():
     assert matched
 
 
+def test_two_arms():
+    matched = False
+    match pep634regex.considering('eggs'):
+        case pep634regex.fullmatch('sp.m'):
+            assert False
+        case pep634regex.fullmatch('eg+s'):
+            matched = True
+    assert matched
+
+
+def test_two_arms_none_match():
+    matched = False
+    match pep634regex.considering('egg'):
+        case pep634regex.fullmatch('sp.m'):
+            assert False
+        case pep634regex.fullmatch('eg+s'):
+            assert False
+        case _:
+            matched = True
+    assert matched
+
+
 def test_match_attribute():
     match = None
     match pep634regex.considering('spam'):
